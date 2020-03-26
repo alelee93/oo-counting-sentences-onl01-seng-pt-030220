204398e7662b3e36833bc30decab9fa4f21a902b
Alejandra I. Lee Vargas <leevargasale@gmail.com>
2020-03-26 00:39:59 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,11 +16,16 @@ class String
 
   def count_sentences
     #self.scan(/[^.\!?]+/).map(&:strip).count
-    
+    binding.pry
     self.split(/\.|\?|\!/).delete_if{|word| word.size < 2}.size
     
     # scan has regex to split string and strip will remove trailing spaces.
     
     
   end
+  
 end
+sentences = "My name is Alejandra.Today is Friday!! Do you like to code?"
+
+sentences.count_sentences
+
